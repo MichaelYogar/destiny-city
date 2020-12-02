d83ec6db5ca048e8dd895a158612310f75433986
Michael Yogar <myogar@uoguelph.ca>
2020-12-02 14:56:10 -0500

    updating passport config file

--- index.js
+++ index.js
@@ -3,39 +3,75 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const bcrypt = require("bcrypt");
+const session = require("express-session");
 const db = require("./db");
+const passport = require("passport");
+const initializePassport = require("./passportConfig");
+const { checkAuthenticated } = require("./middleware");
 
 const port = 5000 || process.env.PORT;
 require("dotenv").config();
 
-// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
-
-// parse application/json
 app.use(bodyParser.json());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+initializePassport(passport);
 
-app.post("/login", async (req, res) => {
-  console.log(req.body);
-  res.send("this is working");
+/* Handle Logout */
+app.get("/logout", (req, res) => {
+  console.log("I am Logout");
+  req.session.destroy(function () {
+    res.clearCookie("connect.sid");
+    res.redirect("/");
+  });
+});
+
+// once done adding react router and just doing some tests in front end, test logout and ten test adding middleware to post login route
+
+app.get("/login", (req, res) => {
+  console.log(req.session);
+  res.json(req.session);
+});
+
+app.post("/login", (req, res, next) => {
+  passport.authenticate("local", (err, user, info) => {
+    if (err) throw err;
+    if (!user) res.send("No User Exists");
+    else {
+      req.logIn(user, (err) => {
+        if (err) throw err;
+        res.send("Successfully Authenticated");
+        console.log(req.user);
+      });
+    }
+  })(req, res, next);
 });
 
 app.post("/register", async (req, res) => {
-  const { email, password } = req.body;
+  const { username, password } = req.body;
   let hashedPassword = await bcrypt.hash(password, 10);
   const result = db.query(
     "INSERT INTO users (username, password) VALUES ($1, $2)",
-    [email, password]
+    [username, hashedPassword]
   );
   res.send("this is working");
 });
 
-// app.get("/", async (req, res) => {
-//   const data = await axios.get(
-//     "https://api.teleport.org/api/countries/iso_alpha2:US/admin1_divisions/geonames:CA/"
-//   );
-//   console.log(data);
-//   res.send("hello world");
-// });
+app.get("/", async (req, res) => {
+  const data = await axios.get(
+    "https://api.teleport.org/api/countries/iso_alpha2:US/admin1_divisions/geonames:CA/"
+  );
+  console.log(data);
+  res.send("hello world");
+});
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
--- passportConfig.js
+++ passportConfig.js
@@ -7,7 +7,6 @@ const initialize = (passport) => {
 
   const authenticateUser = async (username, password, done) => {
     console.log(username, password);
-    console.log("i get here akl;sfj;lkadjfl;a");
     try {
       const results = await db.query(
         `SELECT * FROM users WHERE username = $1`,
@@ -22,19 +21,17 @@ const initialize = (passport) => {
             console.log(err);
           }
           if (isMatch) {
-            console.log("i get here, there is a match");
+            console.log("Password is correct");
             return done(null, user);
           } else {
             //password is incorrect
-            console.log("i get here, password is incorrect");
-            return done(null, false, { message: "Password is incorrect" });
+            console.log("Password is incorrect");
+            return done(null, false);
           }
         });
       } else {
         // No user
-        return done(null, false, {
-          message: "No user with that username address",
-        });
+        return done(null, false);
       }
     } catch (err) {
       console.log(err);
@@ -47,23 +44,17 @@ const initialize = (passport) => {
       authenticateUser
     )
   );
-  // Stores user details inside session. serializeUser determines which data of the user
-  // object should be stored in the session. The result of the serializeUser method is attached
-  // to the session as req.session.passport.user = {}. Here for instance, it would be (as we provide
-  //   the user id as the key) req.session.passport.user = {id: 'xyz'}
   passport.serializeUser((user, done) => done(null, user.id));
 
-  // In deserializeUser that key is matched with the in memory array / database or any data resource.
-  // The fetched object is attached to the request object as req.user
-
-  passport.deserializeUser((id, done) => {
-    db.query(`SELECT * FROM users WHERE id = $1`, [id], (err, results) => {
-      if (err) {
-        return done(err);
-      }
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const results = await db.query(`SELECT * FROM users WHERE id = $1`, [id]);
       console.log(`ID is ${results.rows[0].id}`);
       return done(null, results.rows[0]);
-    });
+    } catch (err) {
+      console.log(err);
+      return done(err);
+    }
   });
 };
 
