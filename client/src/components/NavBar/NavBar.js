8aea5ff633c5a521f6a1e807de2ed316d9ac2df1
Michael Yogar <myogar@uoguelph.ca>
2020-12-03 18:45:33 -0500

    made navbar responsive and added button

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,52 +1,92 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { MdFingerprint } from "react-icons/md";
+import { MdWork } from "react-icons/md";
 import { FaBars, FaTimes } from "react-icons/fa";
+import { Button } from "../Button/Button";
+import useWindowDimensions from "../../hooks/useWindowDimensions";
+import { IconContext } from "react-icons/lib";
 import "./NavBar.scss";
 
 function NavBar() {
   const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+  const { height, width } = useWindowDimensions();
 
   const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  useEffect(() => {
+    const showButton = () => {
+      if (width <= 960) {
+        setButton(false);
+      } else {
+        setButton(true);
+      }
+    };
+    showButton();
+    window.addEventListener("resize", showButton);
+    return () => {
+      window.removeEventListener("resize", showButton);
+    };
+  }, [width, height]);
 
   return (
     <>
-      <div className="NavBar">
-        <div className="NavBar-container container">
-          <Link to="/" className="NavBar-logo">
-            <MdFingerprint className="NavBar-icon" />
-            WhereToWork
-          </Link>
-          <div className="menu-icon" onClick={handleClick}>
-            {click ? <FaTimes /> : <FaBars />}
+      <IconContext.Provider value={{ color: "#fff" }}>
+        <nav className="navbar">
+          <div className="navbar-container container">
+            <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+              <MdWork className="navbar-icon" />
+              WhereToWork
+            </Link>
+            <div className="menu-icon" onClick={handleClick}>
+              {click ? <FaTimes /> : <FaBars />}
+            </div>
+            <ul className={click ? "nav-menu active" : "nav-menu"}>
+              <li className="nav-item">
+                <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to="/services"
+                  className="nav-links"
+                  onClick={closeMobileMenu}
+                >
+                  Featured
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to="/products"
+                  className="nav-links"
+                  onClick={closeMobileMenu}
+                >
+                  About
+                </Link>
+              </li>
+              <li className="nav-btn">
+                {button ? (
+                  <Link to="/sign-up" className="btn-link">
+                    <Button buttonStyle="btn--outline">SIGN UP</Button>
+                  </Link>
+                ) : (
+                  <Link to="/sign-up" className="btn-link">
+                    <Button
+                      buttonStyle="btn--outline"
+                      buttonSize="btn--mobile"
+                      onClick={closeMobileMenu}
+                    >
+                      SIGN UP
+                    </Button>
+                  </Link>
+                )}
+              </li>
+            </ul>
           </div>
-          <ul className={click ? "nav-menu active" : "nav-menu"}>
-            <li className="nav-item">
-              <Link to="/" className="nav-links">
-                Home
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/" className="nav-links">
-                Login
-              </Link>
-            </li>
-            <li className="nav-btn">
-              {button ? (
-                <Link to="/sign-up" className="btn-link">
-                  <Button buttonStyle="btn--outline"> Sign Up</Button>
-                </Link>
-              ) : (
-                <Link to="/sign-up" className="btn-link">
-                  <Button buttonStyle="btn--outline" buttonSize="btn--mobile">
-                    Sign Up
-                  </Button>
-                </Link>
-              )}
-            </li>
-          </ul>
-        </div>
-      </div>
+        </nav>
+      </IconContext.Provider>
     </>
   );
 }
