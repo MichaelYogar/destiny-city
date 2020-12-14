01251d1009df2cd9d7a1790323334dcaf428ad43
Michael Yogar <myogar@uoguelph.ca>
2020-12-14 17:54:54 -0500

    correctly able to get city salaries by using embedded linked resources

--- routes/api.js
+++ routes/api.js
@@ -18,4 +18,22 @@ router.post("/cities", async (req, res) => {
   }
 });
 
+router.post("/cities/salaries", async (req, res) => {
+  const city = req.body.City;
+  let url = `https://api.teleport.org/api/cities/?search=${city}
+                &embed=city:search-results/city:item/city:urban_area/ua:salaries`;
+  const encodedURI = encodeURI(url);
+
+  try {
+    const result = await axios.get(encodedURI);
+    const salaryArray =
+      result.data._embedded["city:search-results"][0]["_embedded"]["city:item"][
+        "_embedded"
+      ]["city:urban_area"]["_embedded"]["ua:salaries"]["salaries"];
+    res.send(salaryArray);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 module.exports = router;
