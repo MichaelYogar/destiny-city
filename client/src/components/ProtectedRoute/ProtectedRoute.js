e95aee03681fe27745105defdbacc02eb1473183
Michael Yogar <myogar@uoguelph.ca>
2020-12-20 17:15:15 -0500

    adding protected route component for routes that require users to sign in

---
+++ client/src/components/ProtectedRoute/ProtectedRoute.js
@@ -0,0 +1,24 @@
+import React, { useContext } from "react";
+import { Route, Redirect } from "react-router-dom";
+import AuthContext from "../../contexts/AuthContext";
+
+function ProtectedRoute({ component: Component, ...rest }) {
+  const { isAuth } = useContext(AuthContext);
+
+  return (
+    <Route
+      {...rest}
+      render={(props) => {
+        console.log(props.location);
+        if (isAuth) return <Component />;
+        return (
+          <Redirect
+            to={{ pathname: "/login", state: { from: props.location } }}
+          />
+        );
+      }}
+    />
+  );
+}
+
+export default ProtectedRoute;
