5fe40ebe255e4a7213bb0bfbd8115d6492a6f352
Michael Yogar <myogar@uoguelph.ca>
2020-12-29 16:35:15 -0500

    updating index.js for server

--- index.js
+++ index.js
@@ -1,58 +1,25 @@
-const axios = require("axios").default;
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const bcrypt = require("bcrypt");
-const session = require("express-session");
 const db = require("./db");
 const routes = require("./routes");
-const passport = require("passport");
-const initializePassport = require("./passportConfig");
-const { checkAuthenticated } = require("./middleware");
+const cors = require("cors");
 
 const port = 5000 || process.env.PORT;
 require("dotenv").config();
 
 /* ---------- MIDDLEWARE ---------- */
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-  })
-);
-
-/* ---------- PASSPORT ---------- */
-app.use(passport.initialize());
-app.use(passport.session());
-initializePassport(passport);
-app.post("/login", (req, res, next) => {
-  passport.authenticate("local", (err, user, info) => {
-    if (err) throw err;
-    if (!user) res.send("No User Exists");
-    else {
-      req.logIn(user, (err) => {
-        if (err) throw err;
-        res.send("Successfully Authenticated");
-      });
-    }
-  })(req, res, next);
-});
-
-app.post("/register", async (req, res) => {
-  const { username, password } = req.body;
-  let hashedPassword = await bcrypt.hash(password, 10);
-  const result = db.query(
-    "INSERT INTO users (username, password) VALUES ($1, $2)",
-    [username, hashedPassword]
-  );
-  res.send("this is working");
-});
 
 /* ---------- ROUTES ---------- */
 app.use(routes);
+
+app.get("/test", (req, res) => {
+  console.log(req.session);
+  res.send("hello world");
+});
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
