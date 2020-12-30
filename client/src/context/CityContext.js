c6a3b4311630824a1d89bf65d236b9c960075826
Michael Yogar <myogar@uoguelph.ca>
2020-12-30 14:07:07 -0500

    cleaning up files that have been refactored

--- client/src/context/CityContext.js
+++
@@ -1,3 +0,0 @@
-import { createContext } from "react";
-const CityContext = createContext("Toronto");
-export default CityContext;
--- client/src/reducers/auth-actions.js
+++
@@ -1,2 +0,0 @@
-export const UPDATE_TOKEN = "UPDATE_TOKEN";
-export const UPDATE_AUTH = "UPDATE_AUTH";
