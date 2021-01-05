dbfa047c48830d3a795fc67b06ddd958aa8f7c37
michaelyogar <myogar@uoguelph.ca>
2021-01-05 08:58:16 -0500

    user table

---
+++ db/database.sql
@@ -0,0 +1,10 @@
+-- create database if haven't already
+
+-- create extension if not exists "uuid-ossp";
+CREATE TABLE users(
+  user_id uuid DEFAULT uuid_generate_v4(),
+  user_name VARCHAR(255) NOT NULL,
+  user_email VARCHAR(255) NOT NULL UNIQUE,
+  user_password VARCHAR(255) NOT NULL,
+  PRIMARY KEY(user_id)
+);
